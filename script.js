f64466efa6e1c639844def7a287bc1bfabfafc69
Hamza Baig <mail.hamzabaig@gmail.com>
2023-07-24 15:43:09 +0500

    Add styles for mobile menu

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-document.addEventListener("DOMContentLoaded", () => {});
+document.addEventListener("DOMContentLoaded", () => {
+  /******    Mobile-menu       ******/
+
+  document.querySelector(".menu").addEventListener("click", () => {
+    document.querySelector("#navbar").classList.add("nav-mbl");
+    document.querySelector("#navbar").classList.remove("nav-mbl-inactive");
+    document.querySelector(".close-menu").style.display = "block";
+    document.querySelector(".menu").style.display = "none";
+  });
+  document.querySelector(".close-menu").addEventListener("click", () => {
+    document.querySelector("#navbar").classList.add("nav-mbl-inactive");
+    document.querySelector("#navbar").classList.remove("nav-mbl");
+    document.querySelector(".close-menu").style.display = "none";
+    document.querySelector(".menu").style.display = "block";
+  });
+});
