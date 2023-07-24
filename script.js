82e6fc502d9ab8304ffbb8dfb0115e910c5e3512
Hamza Baig <mail.hamzabaig@gmail.com>
2023-07-24 20:36:07 +0500

    Fix hamburger menu issues and linter errors

--- script.js
+++ script.js
@@ -1,29 +1,45 @@
-document.addEventListener("DOMContentLoaded", () => {
-  /******    Mobile-menu       ******/
+document.addEventListener('DOMContentLoaded', () => {
+  /** ****    Mobile-menu       ***** */
 
-  document.querySelector(".menu").addEventListener("click", () => {
-    document.querySelector("#navbar").classList.add("nav-mbl");
-    document.querySelector("#navbar").classList.remove("nav-mbl-inactive");
-    document.querySelector(".close-menu").style.display = "block";
-    document.querySelector(".menu").style.display = "none";
+  document.querySelector('.menu').addEventListener('click', () => {
+    document.querySelector('#navbar').classList.add('nav-mbl');
+    document.querySelector('#navbar').classList.remove('nav-mbl-inactive');
+    document.querySelector('.close-menu').style.display = 'block';
+    document.querySelector('.menu').style.display = 'none';
   });
-  document.querySelector(".close-menu").addEventListener("click", () => {
-    document.querySelector("#navbar").classList.add("nav-mbl-inactive");
-    document.querySelector("#navbar").classList.remove("nav-mbl");
-    document.querySelector(".close-menu").style.display = "none";
-    document.querySelector(".menu").style.display = "block";
+  document.querySelector('.close-menu').addEventListener('click', () => {
+    document.querySelector('#navbar').classList.add('nav-mbl-inactive');
+    document.querySelector('#navbar').classList.remove('nav-mbl');
+    document.querySelector('.close-menu').style.display = 'none';
+    document.querySelector('.menu').style.display = 'block';
   });
-  
-  document.querySelector("#portfolio").addEventListener("click", () => {
-    document.querySelector("#navbar").classList.add("nav-mbl-inactive");
-    document.querySelector("#navbar").classList.remove("nav-mbl");
+
+  function updateMenuVisibility() {
+    const windowWidth = window.innerWidth;
+    const menuElement = document.querySelector('.menu');
+    if (windowWidth >= 786) {
+      menuElement.style.display = 'none';
+    } else {
+      menuElement.style.display = 'block';
+    }
+  }
+
+  document.querySelector('.menu1').addEventListener('click', () => {
+    document.querySelector('#navbar').classList.add('nav-mbl-inactive');
+    document.querySelector('#navbar').classList.remove('nav-mbl');
+    document.querySelector('.close-menu').style.display = 'none';
+    updateMenuVisibility();
   });
-  document.querySelector("#about-section").addEventListener("click", () => {
-    document.querySelector("#navbar").classList.add("nav-mbl-inactive");
-    document.querySelector("#navbar").classList.remove("nav-mbl");
+  document.querySelector('.menu2').addEventListener('click', () => {
+    document.querySelector('#navbar').classList.add('nav-mbl-inactive');
+    document.querySelector('#navbar').classList.remove('nav-mbl');
+    document.querySelector('.close-menu').style.display = 'none';
+    updateMenuVisibility();
   });
-  document.querySelector("#contact-section").addEventListener("click", () => {
-    document.querySelector("#navbar").classList.add("nav-mbl-inactive");
-    document.querySelector("#navbar").classList.remove("nav-mbl");
+  document.querySelector('.menu3').addEventListener('click', () => {
+    document.querySelector('#navbar').classList.add('nav-mbl-inactive');
+    document.querySelector('#navbar').classList.remove('nav-mbl');
+    document.querySelector('.close-menu').style.display = 'none';
+    updateMenuVisibility();
   });
 });
