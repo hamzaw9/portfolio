64b424959d20aff79b6a3cb7564822039c2c0665
bhushan354 <127464886+bhushan354@users.noreply.github.com>
2023-07-25 17:39:52 +0530

    linters

--- script.js
+++ script.js
@@ -76,6 +76,16 @@ const projects = [
       "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent",
     technologies: ["HTML", "CSS", "JavaScript"],
   },
-  
+  {
+    id: "project-4",
+    image: "images/project-4.svg",
+    name: "project-4",
+    roles: ["CANOPY", "Back End Dev", "2015"],
+    shortDescription:
+      "A daily selection of privately personalized reads; no accounts or sign-ups required.",
+    longDescription:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent",
+    technologies: ["HTML", "CSS", "JavaScript"],
+  },
 ];
 
