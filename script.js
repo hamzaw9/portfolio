3f3aa9973aaaa37733fb0e14c949489887733878
Hamza Baig <mail.hamzabaig@gmail.com>
2023-07-25 14:54:48 +0500

    Store all of information for all projects using array of objects

--- script.js
+++ script.js
@@ -39,3 +39,52 @@ document.addEventListener("DOMContentLoaded", () => {
     item.addEventListener("click", closeMobileMenu);
   });
 });
+
+/**********    Popup Window       **********/
+
+const projects = [
+  {
+    id: "project-1",
+    image: "images/project-1.svg",
+    name: "project-1",
+    roles: ["CANOPY", "Back End Dev", "2015"],
+    shortDescription:
+      "A daily selection of privately personalized reads; no accounts or sign-ups required.",
+    longDescription:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent",
+    technologies: ["HTML", "CSS", "JavaScript"],
+  },
+  {
+    id: "project-2",
+    image: "images/project-2.svg",
+    name: "project-2",
+    roles: ["CANOPY", "Back End Dev", "2015"],
+    shortDescription:
+      "A daily selection of privately personalized reads; no accounts or sign-ups required.",
+    longDescription:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent",
+    technologies: ["HTML", "CSS", "JavaScript"],
+  },
+  {
+    id: "project-3",
+    image: "images/project-3.svg",
+    name: "project-3",
+    roles: ["CANOPY", "Back End Dev", "2015"],
+    shortDescription:
+      "A daily selection of privately personalized reads; no accounts or sign-ups required.",
+    longDescription:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent",
+    technologies: ["HTML", "CSS", "JavaScript"],
+  },
+  {
+    id: "project-4",
+    image: "images/project-4.svg",
+    name: "project-4",
+    roles: ["CANOPY", "Back End Dev", "2015"],
+    shortDescription:
+      "A daily selection of privately personalized reads; no accounts or sign-ups required.",
+    longDescription:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent",
+    technologies: ["HTML", "CSS", "JavaScript"],
+  },
+];
