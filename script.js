2f5a31116f6d0da79d3876bc500ff6565e8611c3
Hamza Baig <mail.hamzabaig@gmail.com>
2023-07-27 20:49:08 +0500

    Fix linter errors

--- script.js
+++ script.js
@@ -1,72 +1,72 @@
-document.addEventListener("DOMContentLoaded", () => {
+document.addEventListener('DOMContentLoaded', () => {
   /** ****    Mobile-menu       ***** */
 
-  document.querySelector(".menu").addEventListener("click", () => {
-    document.querySelector("#navbar").classList.add("nav-mbl");
-    document.querySelector("#navbar").classList.remove("nav-mbl-inactive");
-    document.querySelector(".close-menu").style.display = "block";
-    document.querySelector(".menu").style.display = "none";
+  document.querySelector('.menu').addEventListener('click', () => {
+    document.querySelector('#navbar').classList.add('nav-mbl');
+    document.querySelector('#navbar').classList.remove('nav-mbl-inactive');
+    document.querySelector('.close-menu').style.display = 'block';
+    document.querySelector('.menu').style.display = 'none';
   });
-  document.querySelector(".close-menu").addEventListener("click", () => {
-    document.querySelector("#navbar").classList.add("nav-mbl-inactive");
-    document.querySelector("#navbar").classList.remove("nav-mbl");
-    document.querySelector(".close-menu").style.display = "none";
-    document.querySelector(".menu").style.display = "block";
+  document.querySelector('.close-menu').addEventListener('click', () => {
+    document.querySelector('#navbar').classList.add('nav-mbl-inactive');
+    document.querySelector('#navbar').classList.remove('nav-mbl');
+    document.querySelector('.close-menu').style.display = 'none';
+    document.querySelector('.menu').style.display = 'block';
   });
 
   function updateMenuVisibility() {
     const windowWidth = window.innerWidth;
-    const menuElement = document.querySelector(".menu");
+    const menuElement = document.querySelector('.menu');
     if (windowWidth >= 786) {
-      menuElement.style.display = "none";
+      menuElement.style.display = 'none';
     } else {
-      menuElement.style.display = "block";
+      menuElement.style.display = 'block';
     }
   }
 
   updateMenuVisibility();
 
-  window.addEventListener("resize", updateMenuVisibility);
+  window.addEventListener('resize', updateMenuVisibility);
 
-  document.querySelector(".menu1").addEventListener("click", () => {
-    document.querySelector("#navbar").classList.add("nav-mbl-inactive");
-    document.querySelector("#navbar").classList.remove("nav-mbl");
-    document.querySelector(".close-menu").style.display = "none";
+  document.querySelector('.menu1').addEventListener('click', () => {
+    document.querySelector('#navbar').classList.add('nav-mbl-inactive');
+    document.querySelector('#navbar').classList.remove('nav-mbl');
+    document.querySelector('.close-menu').style.display = 'none';
     updateMenuVisibility();
   });
-  document.querySelector(".menu2").addEventListener("click", () => {
-    document.querySelector("#navbar").classList.add("nav-mbl-inactive");
-    document.querySelector("#navbar").classList.remove("nav-mbl");
-    document.querySelector(".close-menu").style.display = "none";
+  document.querySelector('.menu2').addEventListener('click', () => {
+    document.querySelector('#navbar').classList.add('nav-mbl-inactive');
+    document.querySelector('#navbar').classList.remove('nav-mbl');
+    document.querySelector('.close-menu').style.display = 'none';
     updateMenuVisibility();
   });
-  document.querySelector(".menu3").addEventListener("click", () => {
-    document.querySelector("#navbar").classList.add("nav-mbl-inactive");
-    document.querySelector("#navbar").classList.remove("nav-mbl");
-    document.querySelector(".close-menu").style.display = "none";
+  document.querySelector('.menu3').addEventListener('click', () => {
+    document.querySelector('#navbar').classList.add('nav-mbl-inactive');
+    document.querySelector('#navbar').classList.remove('nav-mbl');
+    document.querySelector('.close-menu').style.display = 'none';
     updateMenuVisibility();
   });
 
   /** ****    Form Validation       ***** */
-  const form = document.querySelector("#form");
-  const formSubmit = form.querySelector(".form-submit");
-  const name = form.querySelector("#name");
-  const email = form.querySelector("#email");
-  const message = form.querySelector("#message");
-  const showError = form.querySelector("span");
+  const form = document.querySelector('#form');
+  const formSubmit = form.querySelector('.form-submit');
+  const name = form.querySelector('#name');
+  const email = form.querySelector('#email');
+  const message = form.querySelector('#message');
+  const showError = form.querySelector('span');
 
   function emailLowerCase() {
     const emailValue = email.value;
     if (emailValue.toLowerCase() === emailValue) {
-      showError.innerText = "";
+      showError.innerText = '';
       return true;
     }
-    showError.classList.add("error");
+    showError.classList.add('error');
     showError.innerText = `Email should be in lowercase. You need to type like this ${emailValue.toLowerCase()}`;
     return false;
   }
 
-  formSubmit.addEventListener("click", (event) => {
+  formSubmit.addEventListener('click', (event) => {
     event.preventDefault();
 
     if (emailLowerCase()) {
@@ -76,12 +76,12 @@ document.addEventListener("DOMContentLoaded", () => {
 
   /** ****    Data Storage in Browser       ***** */
 
-  const storedData = localStorage.getItem("userData");
+  const storedData = localStorage.getItem('userData');
   const convertStoredData = storedData ? JSON.parse(storedData) : {};
   if (convertStoredData) {
-    name.value = convertStoredData.name || "";
-    email.value = convertStoredData.email || "";
-    message.value = convertStoredData.message || "";
+    name.value = convertStoredData.name || '';
+    email.value = convertStoredData.email || '';
+    message.value = convertStoredData.message || '';
   }
   function updateFormData() {
     const nameValue = name.value;
@@ -93,9 +93,9 @@ document.addEventListener("DOMContentLoaded", () => {
       message: messageValue,
     };
     const jsonFormData = JSON.stringify(formData);
-    localStorage.setItem("userData", jsonFormData);
+    localStorage.setItem('userData', jsonFormData);
   }
-  name.addEventListener("input", updateFormData);
-  email.addEventListener("input", updateFormData);
-  message.addEventListener("input", updateFormData);
+  name.addEventListener('input', updateFormData);
+  email.addEventListener('input', updateFormData);
+  message.addEventListener('input', updateFormData);
 });
